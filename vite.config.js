3e844313b0ea3558ecbd336c6aafac047b51cbcd
davidsmarcelino <67438448+davidsmarcelino@users.noreply.github.com>
2025-05-24 21:54:30 -0300

    nps-dashboard

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 export default defineConfig({
+    base: "/nps-dashboard/",
     plugins: [react()],
     resolve: {
         alias: {
